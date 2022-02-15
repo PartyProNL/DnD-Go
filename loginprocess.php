8ca62041bbc84525d726b3a8bdb02ae414ca651d
Yourie Scheepers <v5in1-yourie@6l-ltinf16.atlas.atlascollege.nl>
2022-02-15 11:24:46 +0100

    Fixed loginprocess.php voor douwe

--- loginprocess.php
+++ loginprocess.php
@@ -1,35 +1,23 @@
-<?php
-   ob_start();
-   session_start();
-?>
-
 <html lang = "en">
-
    <head>
       <title>DnDGo login</title>
       <link href = "styles/style.css" rel = "stylesheet">
    </head>
-
    <body>
      <?php
      $db = new PDO('sqlite:database/dndgo');
      $password = $_POST["password"];
      $email = $_POST["email"];
-     $sql = "SELECT COUNT(user_id) AS check FROM users WHERE email = '".$email."' AND WHERE password = '".$password."'";
-    $check = $db->query($sql);
+     $sql = "SELECT COUNT(user_id) AS aantal FROM users WHERE email = '".$email."' AND password = '".$password."'";
+     $check = $db->query($sql);
 
-    $aantalGevondenAccounts = 0;
-    foreach ($check as $row) {
-      $aantalGevondenAccounts = $row['check'];
-    }
-    echo $aantalGevondenAccounts;
-    $db = NULL;
+     $aantalGevondenAccounts = 0;
+     foreach ($check as $row) {
+       $aantalGevondenAccounts = $row['aantal'];
+     }
+     echo $aantalGevondenAccounts;
+     $db = NULL;
      ?>
-
+    Click <a href = "home.html" tite = "Logout">here</a> to return to the homepage.
   </body>
-         Click <a href = "home.html" tite = "Logout">here</a> to return to the homepage.
-
-      </div>
-
-   </body>
 </html>
