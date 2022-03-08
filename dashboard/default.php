66d580893b4c0a9e6ac9254149a4151da7154b7c
Yourie Scheepers <v5in1-yourie@6l-ltinf16.atlas.atlascollege.nl>
2022-03-08 11:38:03 +0100

    Finished default page for dashboard

--- dashboard/default.php
+++ dashboard/default.php
@@ -9,6 +9,47 @@
     <link rel="stylesheet" href="./styles/style.css">
   </head>
   <body>
+    <?php
+    session_start();
+
+    if(array_key_exists("session", $_SESSION)) {
+      // De session token ophalen
+      $session_token = $_SESSION["session"];
+
+      // Email Ophalen
+      $session_email = $_SESSION["session_email"];
+
+      // Checken of de session token wel in de database zit
+      $db = new PDO('sqlite:../database/dndgo');
+      $sql = "SELECT COUNT(session) AS aantal FROM users WHERE session = '".$session_token."' AND email = '".$session_email."'";
+      $resultaat = $db->query($sql);
+      $db = null;
+
+      // Ophalen hoeveel accounts er zijn gevonden met dezelfde session token
+      $aantalGevondenAccounts = 0;
+      foreach ($resultaat as $row) {
+        $aantalGevondenAccounts = $row['aantal'];
+      }
+
+      if($aantalGevondenAccounts == 0) {
+        // Session token weghalen
+        unset($_SESSION['session']);
+
+        // Bericht meegeven dat de sessie verlopen is
+        $_SESSION["session_ended"] = "Je sessie is verlopen. Log opnieuw in.";
+
+        // Doorsturen naar de login pagina
+        header('Location: ../login.php');
+      }
+    } else {
+      // Bericht meegeven dat de sessie verlopen is
+      $_SESSION["session_ended"] = "Je sessie is verlopen. Log opnieuw in.";
+
+      // Doorsturen naar de login pagina
+      header('Location: ../login.php');
+    }
+     ?>
+
     <header>
       <div class="header-title-container">
         <div class="header-logo-container">
